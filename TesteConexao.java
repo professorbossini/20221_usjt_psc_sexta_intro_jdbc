425b1da3d05bd452a7ed25cff6065e0bab96f6b7
Rodrigo Bossini <professorbossini@gmail.com>
2022-06-03 11:21:22 -0300

    Apostila 0x: bloco de codigo 2.6.1 implementado

---
+++ TesteConexao.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class TesteConexao{
+    public static void main(String[] args) {
+        //try/catch
+        try{
+            Connection minhaConexao = DriverManager.getConnection(
+                //String de conexão
+                "jdbc:mysql://localhost:3306/20221_usjt_psc_sexta_pessoas",
+                "root",
+                "1234"    
+            );
+            System.out.println("Conexão OK!");
+            minhaConexao.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Conexão OK");
+        }
+    }
+}
